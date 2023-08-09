6f95b9176d8dfd30be6e46e37248b1ec5207455f
uyeol <u10e822@gmail.com>
2023-08-09 21:47:54 +0900

    23/08/09 : SutdentApp 과제 완료

--- H20230809/src/co/yedam/StudentApp.java
+++ H20230809/src/co/yedam/StudentApp.java
@@ -31,6 +31,8 @@ public class StudentApp {
 					std.score = stdScore;
 					
 					students[idx++] = std;
+					
+					System.out.println("등록이 완료되었습니다.");
 					break;
 				case 2:
 					System.out.println("학생 점수 조회입니다.");
@@ -38,7 +40,7 @@ public class StudentApp {
 					System.out.println("학생 이름을 입력하세요 : ");
 					String targetName = sc.nextLine();
 					
-					for (int i = 0; i < students.length; i++) {
+					for (int i = 0; i < idx; i++) {
 						if (students[i].name.equals(targetName)) {
 							System.out.printf(
 									"'%s'학생의 점수는 %d점 입니다.\n", 
@@ -49,16 +51,23 @@ public class StudentApp {
 					break;
 				case 3:
 					System.out.println("학생 목록 조회입니다.");
+					System.out.println("===========================");
 					
-					for (int i = 0; i < students.length; i++) {
+					for (int i = 0; i < idx; i++) {
 						System.out.printf(
-								"학생%d. 이름 : %s, 점수 : %d", 
+								"학생%d. 이름 : %s, 점수 : %d\n", 
 								i+1, students[i].name, students[i].score);
 					}
+					
+					System.out.println("===========================");
 					break;
 				case 4:
 					System.out.println("프로그램을 종료합니다.");
 					run = false;
+					break;
+				default:
+					System.out.println("번호를 다시 입력해주세요.");
+					break;
 			
 			}
 		}
